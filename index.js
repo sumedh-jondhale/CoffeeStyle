b018f55b4717445bc4e27ed593f1c69e00a49b19
sumedh-jondhale <sumedhjondhale2017@gmail.com>
2022-03-11 15:32:23 +0530

    coffee

---
+++ index.js
@@ -0,0 +1,13 @@
+function myFunction(){
+
+    var x = document.getElementById("menu");
+    
+        if(x.style.display =="block"){
+            x.style.display="none";
+    
+        }
+        else{
+            x.style.display="block";
+        }
+    
+    }
